0e9c3c2b124f8b4284332db93eefd8af9af0e598
Kelsea W <kelsea.wessel@google.com>
2026-05-21 09:16:16 -0500

    Added comments to explain function

--- Testblock.js
+++ Testblock.js
@@ -1,11 +1,13 @@
 //Create a recursive function that reverses a string. For example, take "hello" and make it output "olleh"
 function reverseString(word){
     if (word === ""){
-        return word; //base case
+        return word; //base case - designed to stop the function if no value is present
     }
     return reverseString
-        (word.substring(1)) + word[0];
+        (word.substring(1)) + word[0]; // Designed to take the letter in position 1 from the input string and places it in position 0 in the output
 }
 
 console.log(reverseString("test"));
 console.log(reverseString("This is the worst idea"));
+
+
