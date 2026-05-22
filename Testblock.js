37769f7c3453ace1307a421659de5e40e6abb5f4
Kelsea W <kelsea.wessel@google.com>
2026-05-22 09:07:21 -0500

    Added an object for practice

--- Testblock.js
+++ Testblock.js
@@ -8,6 +8,33 @@ function reverseString(word){
 }
 
 console.log(reverseString("test"));
-console.log(reverseString("This is the worst idea"));
+console.log(reverseString("This is the worst idea")); //these two strings should show as reversed in the console log
 
 
+// Object Oriented Programming Example
+class User{
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+        this.isActive = true;
+    }
+
+    updateEmail(newEmail){
+        this.email = newEmail;
+    }
+
+    deactivate(){
+        this.isActive = false;
+    }
+}
+
+//Create a new user
+
+const user1 = new User("John Doe", "john.doe@example.com");
+console.log(user1.name); //Output John Doe
+
+user1.updateEmail("john.new@example.com");
+console.log(user1.email); // Output john.new@example.com
+
+user1.deactivate();
+console.log(user1.isActive) //Output should show false
